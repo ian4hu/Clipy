6deb917223a47b2f19d37ed1d37650348b8430d9
xuge <jixu.hjx@antfin.com>
2019-11-27 00:36:34 +0800

    pm

--- Clipy.xcworkspace/xcshareddata/swiftpm/Package.swift
+++ Clipy.xcworkspace/xcshareddata/swiftpm/Package.swift
@@ -14,11 +14,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/tadija/AEXML.git", from: "4.5.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.4"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
-        .package(url: "https://github.com/thii/SwiftHEXColors.git", from: "1.3.1")
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.5.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.4")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/thii/SwiftHEXColors.git", .upToNextMajor(from: "1.3.1")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxRealm", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
