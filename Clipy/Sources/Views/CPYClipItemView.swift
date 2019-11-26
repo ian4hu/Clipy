6619e5e51741734cba2502622a3211a463dcc495
xuge <jixu.hjx@antfin.com>
2019-11-27 01:25:30 +0800

    swiftui

---
+++ Clipy/Sources/Views/CPYClipItemView.swift
@@ -0,0 +1,25 @@
+// 
+//  CPYClipItemView.swift
+//
+//  Clipy
+//  GitHub: https://github.com/clipy
+//  HP: https://clipy-app.com
+// 
+//  Created by 胡继续 on 2019/11/27.
+// 
+//  Copyright © 2015-2019 Clipy Project.
+//
+
+import SwiftUI
+
+struct CPYClipItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CPYClipItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        CPYClipItemView()
+    }
+}
